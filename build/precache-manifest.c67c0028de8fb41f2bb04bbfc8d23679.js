2ca76e335fe14dabf2fe2aadad64d8eb9a0d394d
Rolando Morales <rolando.moralesjr21@gmail.com>
2020-10-27 13:39:59 -1000

    17th change

---
+++ build/precache-manifest.c67c0028de8fb41f2bb04bbfc8d23679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50467bb71367c563b5483647ff0b2ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7f0a6920d2d2240cbc",
+    "url": "/static/css/main.4b9503c0.chunk.css"
+  },
+  {
+    "revision": "8a5866633338a42d33da",
+    "url": "/static/js/2.c520405c.chunk.js"
+  },
+  {
+    "revision": "0e04a221c93a4a0cded169214c9ba847",
+    "url": "/static/js/2.c520405c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7f0a6920d2d2240cbc",
+    "url": "/static/js/main.d027bc83.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  },
+  {
+    "revision": "99752674b66576a5329e63ce11eb83da",
+    "url": "/static/media/gc.99752674.jpg"
+  }
+]);
