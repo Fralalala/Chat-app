c1a74ee9d25fe2932d881dfdd5474728f9a93956
Rolando Morales <rolando.moralesjr21@gmail.com>
2020-10-26 14:58:53 -1000

    9th server change

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const favicon = require("express-favicon");
 const path = require("path");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 io.on("connection", (socket) => {
   let myPath = "";
@@ -42,8 +43,10 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
 app.listen(port);
