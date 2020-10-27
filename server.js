f3d75ee41f47f3db3468f24e930310b5b6057717
Rolando Morales <rolando.moralesjr21@gmail.com>
2020-10-26 17:25:01 -1000

    11th server change

--- server.js
+++ server.js
@@ -39,14 +39,25 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(favicon(__dirname + "/build/favicon.ico"));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.use(favicon(__dirname + "/build/favicon.ico"));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, "client", "build")));
+// app.get("/ping", function (req, res) {
+//   return res.send("pong");
+// });
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 app.listen(port);
