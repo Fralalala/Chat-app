df183f1b15560f3f8be230ad1d01d0d88237ecc7
Rolando Morales <rolando.moralesjr21@gmail.com>
2020-10-27 12:19:27 -1000

    16th change

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
 const http = require("http");
 const app = express();
@@ -38,26 +37,11 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(favicon(__dirname + "client/build/favicon.ico"));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, "client", "build")));
-// app.get("/ping", function (req, res) {
-//   return res.send("pong");
-// });
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "build")));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port);
