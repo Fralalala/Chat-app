c4d008c5551a094bbe9c1dc3740eafa492d43daa
Rolando Morales <rolando.moralesjr21@gmail.com>
2020-10-26 14:18:19 -1000

    4th server change

--- server.js
+++ server.js
@@ -38,15 +38,13 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(favicon(__dirname + "/build/favicon.ico"));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 server.listen(port, () => console.log(`server listening on port 8k and `));
