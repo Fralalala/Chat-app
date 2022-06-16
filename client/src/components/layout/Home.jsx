8ae882395434fcf5860117282bd40594f3646237
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-06-15 18:29:07 -1000

    Aws testing

--- client/src/components/layout/Home.jsx
+++ client/src/components/layout/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <Fragment>
            <Messaging /> 
+           <img src="https://randmsgbucket.s3.us-west-1.amazonaws.com/2cb59578-dd6a-49a7-9894-49e290c0a1cd.jpg" alt="Alt_text_img" />
         </Fragment>
     )
 }
